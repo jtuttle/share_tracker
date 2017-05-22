825511595f22f9db2e1a688527c1c4543010d30d
John Tuttle <jtuttle@craw.cc>
2017-05-22 17:05:22 -0400

    clean up migration

--- db/migrate/20170521181712_create_users_table.rb
+++ db/migrate/20170521181712_create_users_table.rb
@@ -3,10 +3,11 @@ class CreateUsersTable < ActiveRecord::Migration[5.1]
     execute 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'
     
     create_table :users do |t|
+      t.timestamps
+      t.uuid :public_identifier, null: :false, default: 'uuid_generate_v4()'
       t.text :username, null: :no
       t.text :password, null: :no
       t.text :salt, null: :no
-      t.uuid :public_identifier, null: :false, default: 'uuid_generate_v4()'
     end
   end
 
